1dbec515308175cddfa331a75aca1266ae17ceed
tseop <77163842+seop-kim@users.noreply.github.com>
2023-10-24 13:04:55 +0900

    feat: add model

---
+++ src/main/java/com/techit/wiseapp/model/WiseModel.java
@@ -0,0 +1,22 @@
+package com.techit.wiseapp.model;
+
+public class WiseModel {
+    String sentence;
+    String author;
+
+    public String getSentence() {
+        return sentence;
+    }
+
+    public void setSentence(String sentence) {
+        this.sentence = sentence;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
