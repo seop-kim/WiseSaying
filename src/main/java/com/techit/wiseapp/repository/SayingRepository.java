2ec3d5847438a63e0cd996a5e5216ab5921f155b
tseop <77163842+seop-kim@users.noreply.github.com>
2023-10-25 16:36:00 +0900

    feat: 명언 레포 생성

---
+++ src/main/java/com/techit/wiseapp/repository/SayingRepository.java
@@ -0,0 +1,46 @@
+package com.techit.wiseapp.repository;
+
+import com.techit.wiseapp.model.WiseModel;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SayingRepository {
+    private static final Map<Long, WiseModel> sayings = new HashMap<>();
+    private static Long id = 0L;
+
+    private static final SayingRepository instance = new SayingRepository();
+
+    private SayingRepository() {
+
+    }
+
+    public static SayingRepository getInstance() {
+        return instance;
+    }
+
+    public WiseModel save(WiseModel saying) {
+        saying.setId(++id);
+        sayings.put(saying.getId(), saying);
+        return saying;
+    }
+
+    public List<WiseModel> findAll() {
+        return new ArrayList<>(sayings.values());
+    }
+
+    public WiseModel delete(Long id) {
+        return sayings.remove(id);
+
+    }
+
+    public WiseModel findOne(Long id) {
+        return sayings.get(id);
+    }
+
+    public void clear() {
+        id = 0L;
+        sayings.clear();
+    }
+}
