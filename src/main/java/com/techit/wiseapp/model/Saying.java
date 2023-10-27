826667c40ccd038d6ef9c1f2d8d2646960c4c119
tseop <77163842+seop-kim@users.noreply.github.com>
2023-10-27 15:22:01 +0900

    feat: add model field

---
+++ src/main/java/com/techit/wiseapp/model/Saying.java
@@ -0,0 +1,32 @@
+package com.techit.wiseapp.model;
+
+public class Saying {
+
+    Long id;
+    String content;
+    String author;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
